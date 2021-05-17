2d7afd3239e25182f84ab591390d18e7cb7b7b7f
Louise Moesgaard Nielsen <louise@moesgaard.dk>
2021-05-17 12:50:55 +0200

    fixes wordpressService

--- src/services/wordpress.js
+++ src/services/wordpress.js
@@ -1,12 +1,12 @@
+const baseEndpoint = "https://ariadna.dk/kea/Dortes-Training/wp/wp-json/wp/v2/";
 class Wordpress {
-    baseEndpoint = "https://ariadna.dk/kea/Dortes-Training/wp/wp-json/wp/v2/";
 
-    getCategories = async () => {
+    static getCategories = async () => {
         const response = await fetch(baseEndpoint + "categories")
         return await response.json();
     }
 
-    getCategory(id) {
+    static getCategory = async (id) => {
         const response = await fetch(baseEndpoint + `categories/${id}`)
         return await response.json();
     }
